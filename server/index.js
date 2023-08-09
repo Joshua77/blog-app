8ce5f31edcbddcc964c1f74278f34ea4753badff
Joshua77 <jpakinola@gmail.com>
2023-08-09 20:36:40 +0100

    cors permission

--- server/index.js
+++ server/index.js
@@ -16,7 +16,14 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({
+  credentials:true,
+  methods: ['POST', 'GET'],
+  origin: 'https://blog-app-nine-gamma.vercel.app/'
+  // origin:'http://localhost:3000'
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
