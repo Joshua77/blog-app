02d7e28891d889cfc2fb2b08933dfdcdec011075
Joshua77 <jpakinola@gmail.com>
2023-08-09 20:28:24 +0100

    added deployed backend link

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -11,7 +11,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
+    fetch(`https://blog-app-api-nine.vercel.app/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -37,7 +37,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`https://blog-app-api-nine.vercel.app/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
